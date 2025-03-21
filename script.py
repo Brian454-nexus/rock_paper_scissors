dac2cd948833efaac38a49df948774d09965a757
Brian454-nexus <brian.terer@student.moringaschool.com>
2025-03-21 14:29:20 +0300

    Added choices for player

--- script.py
+++ script.py
@@ -3,4 +3,7 @@ import random
 def rock_paper_scissors():
     print("Welcome to Rock, Paper, Scissors!")
 
+    choices = ["rock", "paper", "scissors"]
+    player_choice = input("Enter your choice (rock, paper, scissors): ").lower()
+
    
